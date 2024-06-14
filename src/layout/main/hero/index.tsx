1310eb7d446b41a0930a719d29d2dbdc9a609430
chenmh0901 <1210204146@zust.edu.cn>
2024-06-14 02:14:18 -0400

    update AnimationCard

--- src/layout/main/hero/index.tsx
+++ src/layout/main/hero/index.tsx
@@ -6,7 +6,7 @@ function CardHero() {
       <h3>Pet Animations</h3>
       <div className="_content">
         <p>自己收集的一些使用CSS实现的动画</p>
-        <p>为了方便自己学习,同时标注了笔记</p>
+        <p>为了方便学习,同时标注了笔记</p>
       </div>
     </div>
   )
--- src/layout/main/list/index.tsx
+++ src/layout/main/list/index.tsx
@@ -1,20 +1,20 @@
 import type { IView } from '@/types/view'
 import { ViewType } from '@/types/view'
-import Card from '@/components/card'
+import AnimationCard from '@/components/AnimationCard'
 import './style.css'
 
 function List({ onViewClick }: { onViewClick: (view: IView) => void }) {
   return (
     <div className="list">
-      <Card title="ClockAnimation" content="这是一个酷炫的闹钟展示动画" onClick={() => onViewClick({ label: 'Clock', value: ViewType.Clock })} />
-      <Card title="ImageWallAnimation" content="这是一个好看的照片墙展示动画" onClick={() => onViewClick({ label: 'Image', value: ViewType.Image })} />
-      <Card title="CardAnimation" content="这是一个好看的卡牌选择动画" onClick={() => onViewClick({ label: 'Card', value: ViewType.Card })} />
-      <Card title="CheckOnAnimation" content="这是一个好看的完成打勾进度动画" onClick={() => onViewClick({ label: 'CheckOn', value: ViewType.CheckOn })} />
-      <Card title="MouseFollowAnimation" content="这是一个好看的流水灯动画" onClick={() => onViewClick({ label: 'MouseFollow', value: ViewType.MouseFollow })} />
-      <Card title="BubbleAnimation" content="这是一个好看的气泡背景动画" onClick={() => onViewClick({ label: 'Bubble', value: ViewType.Bubble })} />
-      <Card title="TimeLineAnimation" content="这是一个CSSTimeLine动画" onClick={() => onViewClick({ label: 'TimeLine', value: ViewType.TimeLine })} />
-      <Card title="WaterLightAnimation" content="这是一个好看的流水灯动画" onClick={() => onViewClick({ label: 'WaterLight', value: ViewType.WaterLight })} />
-      <Card title="BugWordAnimation" content="这是一个好看的文字故障动画" onClick={() => onViewClick({ label: 'BugWord', value: ViewType.BugWord })} />
+      <AnimationCard title="ClockAnimation" content="这是一个酷炫的闹钟展示动画" onClick={() => onViewClick({ label: 'Clock', value: ViewType.Clock })} />
+      <AnimationCard title="ImageWallAnimation" content="这是一个好看的照片墙展示动画" onClick={() => onViewClick({ label: 'Image', value: ViewType.Image })} />
+      <AnimationCard title="AnimationCardAnimation" content="这是一个好看的卡牌选择动画" onClick={() => onViewClick({ label: 'AnimationCard', value: ViewType.Card })} />
+      <AnimationCard title="CheckOnAnimation" content="这是一个好看的完成打勾进度动画" onClick={() => onViewClick({ label: 'CheckOn', value: ViewType.CheckOn })} />
+      <AnimationCard title="MouseFollowAnimation" content="这是一个好看的流水灯动画" onClick={() => onViewClick({ label: 'MouseFollow', value: ViewType.MouseFollow })} />
+      <AnimationCard title="BubbleAnimation" content="这是一个好看的气泡背景动画" onClick={() => onViewClick({ label: 'Bubble', value: ViewType.Bubble })} />
+      <AnimationCard title="TimeLineAnimation" content="这是一个CSSTimeLine动画" onClick={() => onViewClick({ label: 'TimeLine', value: ViewType.TimeLine })} />
+      <AnimationCard title="WaterLightAnimation" content="这是一个好看的流水灯动画" onClick={() => onViewClick({ label: 'WaterLight', value: ViewType.WaterLight })} />
+      <AnimationCard title="BugWordAnimation" content="这是一个好看的文字故障动画" onClick={() => onViewClick({ label: 'BugWord', value: ViewType.BugWord })} />
     </div>
   )
 }
